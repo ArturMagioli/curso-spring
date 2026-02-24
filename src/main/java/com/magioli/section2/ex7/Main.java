02c20edd6c60cb642fd1a64624d3aa00fa43b6c8
Artur Magioli <arturdepaula00@gmail.com>
2026-02-24 13:09:03 -0300

    prototype scope

--- src/main/java/com/magioli/section2/ex7/Main.java
+++ src/main/java/com/magioli/section2/ex7/Main.java
@@ -1,6 +1,7 @@
 package com.magioli.section2.ex7;
 
 import com.magioli.section2.ex7.beans.MyService;
+import com.magioli.section2.ex7.beans.UserSession;
 import com.magioli.section2.ex7.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,5 +13,10 @@ public class Main {
         MyService myService2 = context.getBean(MyService.class);
 
         System.out.println(myService1.hashCode() == myService2.hashCode());
+
+        UserSession userSession1 = context.getBean(UserSession.class);
+        UserSession userSession2 = context.getBean(UserSession.class);
+
+        System.out.println(userSession1.getSessionId() == userSession2.getSessionId());
     }
 }
