7651ea509a2802d70eaf6035d42312bf480654da
Artur Magioli <arturdepaula00@gmail.com>
2026-02-26 10:49:30 -0300

    spring boot skeleton

---
+++ src/main/java/com/magioli/section3/backend/src/test/java/com/magioli/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magioli.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
