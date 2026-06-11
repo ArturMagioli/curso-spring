bbb445c82073a4d66e9b9dfdf63f7ca9df3f54c9
Artur Magioli <arturdepaula00@gmail.com>
2026-06-11 13:31:50 -0300

    Mudanca no scheme usando ddl-auto do Hibernate

---
+++ src/main/java/com/magioli/section7/jobportal/src/main/java/com/magioli/jobportal/entity/Contact.java
@@ -0,0 +1,52 @@
+package com.magioli.jobportal.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "contacts")
+@Getter
+@Setter
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
+
+    @Column(name = "name",nullable = false,unique = true)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "user_type", nullable = false, length = 50)
+    private String userType;
+
+    @Column(name = "subject", nullable = false)
+    private String subject;
+
+    @Lob
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @ColumnDefault("'NEW'")
+    @Column(name = "status", nullable = false, length = 20)
+    private String status;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "created_by", nullable = false, length = 20)
+    private String createdBy;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "updated_by", length = 20)
+    private String updatedBy;
+}
