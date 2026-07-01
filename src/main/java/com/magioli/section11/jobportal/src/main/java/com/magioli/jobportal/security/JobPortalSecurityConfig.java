cb51c81a443c7fe9e5b24346107d7de0418b130d
Artur Magioli <arturdepaula00@gmail.com>
2026-07-01 19:31:22 -0300

    hotfix: remocao de import desnecessario

--- src/main/java/com/magioli/section11/jobportal/src/main/java/com/magioli/jobportal/security/JobPortalSecurityConfig.java
+++ src/main/java/com/magioli/section11/jobportal/src/main/java/com/magioli/jobportal/security/JobPortalSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
