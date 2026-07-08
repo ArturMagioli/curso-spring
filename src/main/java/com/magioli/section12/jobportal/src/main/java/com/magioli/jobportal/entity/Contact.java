1241335ac104467c7afb1256cf765c8943ebf55c
Artur Magioli <arturdepaula00@gmail.com>
2026-07-08 10:14:03 -0300

    secao 12 - Database Authentication & CSRF Protection

---
+++ src/main/java/com/magioli/section12/jobportal/src/main/java/com/magioli/jobportal/entity/Contact.java
@@ -0,0 +1,39 @@
+package com.magioli.jobportal.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Table(name = "contacts")
+@Getter
+@Setter
+public class Contact extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
+
+    @Column(name = "name",nullable = false,unique = true)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "user_type", nullable = false, length = 50)
+    private String userType;
+
+    @Column(name = "subject", nullable = false)
+    private String subject;
+
+    @Lob
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @ColumnDefault("'NEW'")
+    @Column(name = "status", nullable = false, length = 20)
+    private String status;
+
+}
---
+++ src/main/java/com/magioli/section12/jobportal/src/main/java/com/magioli/jobportal/security/JobPortalSecurityConfig.java
@@ -0,0 +1,93 @@
+package com.magioli.jobportal.security;
+
+import com.magioli.jobportal.filter.JwtTokenValidatorFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class JobPortalSecurityConfig {
+
+    @Qualifier("publicPaths")
+    private final List<String> publicPaths;
+
+    @Qualifier("securedPaths")
+    private final List<String> securedPaths;
+
+    @Bean
+    SecurityFilterChain customSecurityFilterChain(HttpSecurity http) {
+        return http.csrf(csrfConfig -> csrfConfig.disable())
+                .cors(corsConfig -> corsConfig.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests((requests) -> {
+                    publicPaths.forEach(path -> requests.requestMatchers(path).permitAll());
+                    securedPaths.forEach(path -> requests.requestMatchers(path).authenticated());
+                    requests.anyRequest().denyAll();
+                })
+                .addFilterBefore(new JwtTokenValidatorFilter(publicPaths), BasicAuthenticationFilter.class)
+                .formLogin(flc -> flc.disable())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+//        String password2 = passwordEncoder().encode("Admin@123"); -> maneira de obter o hash
+//        System.out.println(password2);
+        UserDetails user1 = User.builder().username("magioli").password("$2a$10$t9w7FHYAHupuEs91v0O2Iujym/eu5wzlHzZwijl5CXesDfF1rvCv")
+            .roles("USER").build();
+        UserDetails user2 = User.builder().username("admin").password("$2a$10$iDb6SUF1iSk2ykEhLSQVHOJ8vjou9IRuY5x8zcCsYCPixRR4s9tDC")
+            .roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
