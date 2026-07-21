f197542fea949baf5f340360b170326369064839
Artur Magioli <arturdepaula00@gmail.com>
2026-07-21 16:42:29 -0300

    secao 14 - Programacao Orientada a Aspectos

---
+++ src/main/java/com/magioli/section14/jobportal/src/main/java/com/magioli/jobportal/repository/JobPortalUserRepository.java
@@ -0,0 +1,13 @@
+package com.magioli.jobportal.repository;
+
+import com.magioli.jobportal.entity.JobPortalUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JobPortalUserRepository extends JpaRepository<JobPortalUser, Long> {
+
+    Optional<JobPortalUser> readByEmailOrMobileNumber(String email, String mobileNumber);
+
+    Optional<JobPortalUser> findByEmail(String email);
+}
