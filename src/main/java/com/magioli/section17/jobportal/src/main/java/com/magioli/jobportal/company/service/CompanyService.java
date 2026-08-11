b2ee4c0e7280ea512fc1d2076a4829b3d494a6ab
Artur Magioli <arturdepaula00@gmail.com>
2026-08-11 19:15:45 -0300

    secao 17 - Transactions no Spring Data JPA

---
+++ src/main/java/com/magioli/section17/jobportal/src/main/java/com/magioli/jobportal/company/service/CompanyService.java
@@ -0,0 +1,10 @@
+package com.magioli.jobportal.company.service;
+
+import com.magioli.jobportal.dto.CompanyDto;
+
+import java.util.List;
+
+public interface CompanyService {
+
+    List<CompanyDto> getAllCompanies();
+}
---
+++ src/main/java/com/magioli/section17/jobportal/src/main/java/com/magioli/jobportal/contact/service/impl/ContactServiceImpl.java
@@ -0,0 +1,98 @@
+package com.magioli.jobportal.contact.service.impl;
+
+import com.magioli.jobportal.constants.ApplicationConstants;
+import com.magioli.jobportal.contact.service.ContactService;
+import com.magioli.jobportal.dto.ContactRequestDto;
+import com.magioli.jobportal.dto.ContactResponseDto;
+import com.magioli.jobportal.entity.Contact;
+import com.magioli.jobportal.repository.ContactRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ContactServiceImpl implements ContactService {
+
+    private final ContactRepository contactRepository;
+
+    @Override
+    public boolean saveContact(ContactRequestDto contactRequestDto) {
+        boolean result = false;
+        Contact contact = contactRepository.save(transformToEntity(contactRequestDto));
+
+        if (contact != null && contact.getId() != null) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    private Contact transformToEntity(ContactRequestDto contactRequestDto) {
+        Contact contact = new Contact();
+        BeanUtils.copyProperties(contactRequestDto, contact);
+        contact.setStatus("NEW");
+        return contact;
+    }
+
+    @Override
+    public List<ContactResponseDto> fetchNewContactMsgs() {
+        List<Contact> contacts = contactRepository.findContactsByStatusOrderByCreatedAtAsc(ApplicationConstants.NEW_MESSAGE);
+        List<ContactResponseDto> contactResponseDtos = contacts.stream()
+                .map(this::transformToDto)
+                .collect(Collectors.toList());
+        return contactResponseDtos;
+    }
+
+    @Override
+    public List<ContactResponseDto> fetchNewContactMsgsWithSort(String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase("desc")
+                ? Sort.by(sortBy).descending()
+                : Sort.by(sortBy).ascending();
+        List<Contact> contacts = contactRepository.findContactsByStatus(ApplicationConstants.NEW_MESSAGE, sort);
+        List<ContactResponseDto> responseDtos = contacts.stream()
+                .map(this::transformToDto)
+                .collect(Collectors.toList());
+        return responseDtos;
+    }
+
+
+    @Override
+    public Page<ContactResponseDto> fetchNewContactMsgsWithPaginationAndSort(int pageNumber, int pageSize, String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase("desc")
+                ? Sort.by(sortBy).descending()
+                : Sort.by(sortBy).ascending();
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+        Page<Contact> contactPage = contactRepository.findContactsByStatus(ApplicationConstants.NEW_MESSAGE, pageable);
+        Page<ContactResponseDto> responseDtos = contactPage.map(this::transformToDto);
+        return responseDtos;
+    }
+
+    private ContactResponseDto transformToDto(Contact contact) {
+        return new ContactResponseDto(contact.getId(),
+                contact.getName(), contact.getEmail(), contact.getUserType(),
+                contact.getSubject(), contact.getMessage(), contact.getStatus(),
+                contact.getCreatedAt());
+    }
+
+    @Override
+    public boolean closeContactMsg(Long id, String status) {
+        Contact contact = contactRepository.findById(id).orElse(null);
+        if (contact == null) {
+            return false;
+        } else {
+            contact.setStatus(status);
+            contactRepository.save(contact);
+        }
+        return true;
+    }
+}
