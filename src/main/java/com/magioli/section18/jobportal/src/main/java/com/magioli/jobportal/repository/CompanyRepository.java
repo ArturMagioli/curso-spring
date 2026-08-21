be1f7f55c74d0d61a65f94e5eab4e6ef8f0265b5
Artur Magioli <arturdepaula00@gmail.com>
2026-08-21 12:56:38 -0300

    Caffeine para caching

--- src/main/java/com/magioli/section18/jobportal/src/main/java/com/magioli/jobportal/repository/CompanyRepository.java
+++ src/main/java/com/magioli/section18/jobportal/src/main/java/com/magioli/jobportal/repository/CompanyRepository.java
@@ -2,6 +2,7 @@ package com.magioli.jobportal.repository;
 
 import com.magioli.jobportal.entity.Company;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,6 +18,7 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
     @Query("SELECT DISTINCT c FROM Company c JOIN FETCH c.jobs j WHERE j.status = :status")
     List<Company> findAllWithJobsByStatus(@Param("status") String status);
 
+    @Cacheable("companies")
     List<Company> fetchCompaniesWithJobsByStatus(@Param("status") String status);
 
     @Query(value = "SELECT DISTINCT c.* FROM companies c JOIN jobs j ON c.id = j.company_id WHERE j.status = :status",
