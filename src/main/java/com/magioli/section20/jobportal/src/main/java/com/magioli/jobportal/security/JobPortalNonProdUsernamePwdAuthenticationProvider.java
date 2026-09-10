317c2fdd654529465b124b4f671054669af5e41b
Artur Magioli <arturdepaula00@gmail.com>
2026-09-10 11:10:38 -0300

    feat: Bean condicional para autenticacao

--- src/main/java/com/magioli/section20/jobportal/src/main/java/com/magioli/jobportal/security/JobPortalNonProdUsernamePwdAuthenticationProvider.java
+++ src/main/java/com/magioli/section20/jobportal/src/main/java/com/magioli/jobportal/security/JobPortalNonProdUsernamePwdAuthenticationProvider.java
@@ -6,29 +6,25 @@ import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Profile("prod")
+@Profile("!prod")
 @Component
 @RequiredArgsConstructor
-public class JobPortalUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+public class JobPortalNonProdUsernamePwdAuthenticationProvider implements AuthenticationProvider {
 
     private final JobPortalUserRepository jobPortalUserRepository;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public @Nullable Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
-        String pwd = authentication.getCredentials().toString();
         JobPortalUser jobPortalUser = jobPortalUserRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(
                         "User details not found for the user: " + username
@@ -36,11 +32,7 @@ public class JobPortalUsernamePwdAuthenticationProvider implements Authenticatio
         List<SimpleGrantedAuthority> authorities = List.of(
                 new SimpleGrantedAuthority(jobPortalUser.getRole().getName())
         );
-        if (passwordEncoder.matches(pwd, jobPortalUser.getPasswordHash())) {
-            return new UsernamePasswordAuthenticationToken(jobPortalUser, null, authorities);
-        } else {
-            throw new BadCredentialsException("Invalid password!");
-        }
+        return new UsernamePasswordAuthenticationToken(jobPortalUser, null, authorities);
     }
 
     @Override
